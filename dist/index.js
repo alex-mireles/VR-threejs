3ecb0ed351500454b9c9a30425d1c3992f818182
Alex Mireles <hyperflame1@gmail.com>
2022-01-13 12:39:22 -0500

    back to basics

--- dist/index.js
+++ dist/index.js
@@ -4,57 +4,30 @@ const canvas = document.querySelector('canvas.webgl')
 
 import { VRButton } from 'https://unpkg.com/three@0.127.0/examples/jsm/webxr/VRButton.js';
 
-// Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const textureLoader = new THREE.TextureLoader()
-const myTexture = textureLoader.load('coolTex.jpg')
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// Object
-const geometry = new THREE.BoxGeometry(3,3,3)
-const material = new THREE.MeshBasicMaterial({
-    map: myTexture
-})
-const boxMesh = new THREE.Mesh(geometry,material)
-scene.add(boxMesh)
-// scene.add(sphereMesh)
-boxMesh.position.x = 0
-boxMesh.position.y = 0.8
-geometry.center()
-// Sizes
-const sizes = {
-    width:window.innerWidth,
-    height:window.innerHeight
-}
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-// Renderer gets updated each time window is resized
-window.addEventListener('resize', onWindowResize);
+camera.position.z = 5;
 
-function onWindowResize() {
+function animate() {
+    requestAnimationFrame( animate );
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-}
+    renderer.render( scene, camera );
+};
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
-camera.position.z = 3
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-
-controls.enableZoom = false;
-controls.enableDamping = true
-
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    alpha: true,
-})
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+animate();
 
 document.body.appendChild( VRButton.createButton( renderer ) );
 renderer.xr.enabled = true;
